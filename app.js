9132eefcc26989cc6dc1f9a9aa8faa6f80bb857a
feedmefear <asatianivaja@gmail.com>
2013-12-30 15:49:07 +0400

    updated javascript

--- app.js
+++ app.js
@@ -10,46 +10,50 @@ function randomsign ()
 
 $(function ()
 {
-  var $polyline = $('svg polyline');
-  function update_points (points)
+  var $polyline = $('svg polyline'),
+  points_attr = '';
+  function update_points (newpoint)
   {
-    $polyline.attr('points', points);
+    points_attr += ' ' + newpoint[0] + ',' + newpoint[1];
+    $polyline.attr('points', points_attr);
   }
 
 
   var $w = $(window).width(),
   $h = $(window).height();
 
-  var startpoint = [randomint(0, $w), randomint(0, $h)];
-
-  var startpointattr = startpoint[0] + ',' + startpoint[1];
-  update_points(startpointattr);
-
   // possible directions
   var pd = [
-    [+1, +1],
-    [+1,  0],
-    [+1, -1],
-    [ 0, +1],
-    [ 0, -1],
-    [-1, +1],
-    [-1,  0],
-    [-1, -1]
+    [-1, -1], [ 0, -1], [+1, -1],
+    [-1,  0],           [+1,  0],
+    [-1, +1], [ 0, +1], [+1, +1]
   ];
+  /*
+  035
+  247
+  pd.splice(0, 1);
+  pd.splice(3, 1);
+  pd.splice(5, 1);
+  console.log(pd);
+  */
+
+  var startpoint = [randomint(0, $w), randomint(0, $h)];
+
+  update_points(startpoint);
 
 
-  (function add_point (prevpoint, prevpointsattr)
+  (function add_point (prevpoint)
   {
+    var multiplier = 5;
 
     var i = randomint(0, pd.length - 1);
-    var newpoint = [prevpoint[0] + pd[i][0], prevpoint[1] + pd[i][1]];
+    var newpoint = [prevpoint[0] + pd[i][0] * multiplier, prevpoint[1] + pd[i][1] * multiplier];
 
-    var newpointsattr = prevpointsattr + ' ' + newpoint[0] + ',' + newpoint[1];
-    update_points(newpointsattr);
+    update_points(newpoint);
 
-    var timerid = setTimeout(add_point, 4, newpoint, newpointsattr);
+    var timerid = setTimeout(add_point, 4, newpoint);
 
-  })(startpoint, startpointattr);
+  })(startpoint);
 
 
 
